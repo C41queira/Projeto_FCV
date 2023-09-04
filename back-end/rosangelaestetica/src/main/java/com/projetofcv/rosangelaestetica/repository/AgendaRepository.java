d7b348127ccaeb625cbb80c4387d7962fcd14d88
Caick Bertin <bertinviana@gmail.com>
2023-09-04 10:04:38 -0300

    Realocando Projeto

---
+++ back-end/rosangelaestetica/src/main/java/com/projetofcv/rosangelaestetica/repository/AgendaRepository.java
@@ -0,0 +1,9 @@
+package com.projetofcv.rosangelaestetica.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetofcv.rosangelaestetica.entity.Agenda;
+
+public interface AgendaRepository extends JpaRepository<Agenda, Long>{
+    
+}
