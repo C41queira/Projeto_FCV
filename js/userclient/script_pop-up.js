c04785a7a0299d0baa939990b5897fb0495e75e1
Fabrício <amorimfabricio580@gmail.com>
2023-11-30 21:19:02 -0300

    add full modal codes

---
+++ js/userclient/script_pop-up.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.popup-container')
+
+function openModal() {
+  modal.classList.add('active')
+}
+
+function closeModal() {
+  modal.classList.remove('active')
+}
+
